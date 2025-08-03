d8c734287c4bb3cbcd22642f8382ba74addab0c0
Abhishek <abhisekchoudhary31@gmail.com>
2025-08-03 23:12:11 +0530

    github data added

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ require('dotenv').config();
 const app = express();
 const port = 4000;
 
+const object ={
+    name :"Abhisehek Choudahary",
+    age :23,
+    email :"cabhisehkchoudhary31@gmail.com",
+    contact:95238923499
+}
+
 app.get('/',(req,res)=>{
     res.send("<h1> Hello,Hitesh sir you are a great teacher.</h1>")
 
@@ -15,8 +22,8 @@ app.get('/login',(req,res)=>{
 
 })
 
-app.get('/signin',(req,res)=>{
-    res.send("<h1> Hello,Hitesh sir you are a great teacher.</h1>")
+app.get('/read',(req,res)=>{
+    res.json(object)
 
 })
 
